b554f82dc3744f611e1831739150a9bd20a85178
SourceXSL <84072118+SourceXSL@users.noreply.github.com>
2021-05-20 12:15:21 -0400

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const Config = require('../config');
 
 
-if (Config.WORKTYPE == 'private') {
+if (Config.WORKTYPE == 'public') {
 	
 Asena.addCommand({pattern: 'alive', fromMe: true, deleteCommand: true,}, (async (message, match) => {
 
